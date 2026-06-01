f335ce4f6c8643914c034248de3c402e9f1e3edd
Alexander Sosedkin <asosedkin@redhat.com>
2026-06-01 18:45:05 +0200

    Add --respond-to=record to match tlsfuzzer expectations

--- src/server.rs
+++ src/server.rs
@@ -1,21 +1,40 @@
 use std::sync::Arc;
 use tokio::net::{TcpListener, TcpStream};
-use tokio::io::{AsyncBufReadExt, AsyncWriteExt};
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, AsyncReadExt};
 
 use gethostname::gethostname;
 use tokio_rustls::rustls::pki_types::PrivatePkcs8KeyDer;
 use tokio_rustls::{TlsAcceptor, server::TlsStream};
 use anyhow::Result;
 
-async fn pongping(tls: &mut TlsStream<TcpStream>) -> std::io::Result<()> {
-    let mut plaintext = String::new();
-    tls.read_line(&mut plaintext).await?;
-    print!("{}", plaintext);
+#[derive(Copy, Clone)]
+enum RespondToMode {
+    Line,   // respond to each line
+    Record, // respond to each read (ApplicationData)
+}
+
+async fn pongping(
+    tls: &mut TlsStream<TcpStream>,
+    mode: RespondToMode,
+) -> std::io::Result<()> {
+    match mode {
+        RespondToMode::Record => {
+            let mut buf = [0u8; 16384];
+            if tls.read(&mut buf).await? == 0 {
+                return Err(std::io::ErrorKind::UnexpectedEof.into());
+            }
+        }
+        RespondToMode::Line => {
+            let mut line = String::new();
+            if tls.read_line(&mut line).await? == 0 {
+                return Err(std::io::ErrorKind::UnexpectedEof.into());
+            }
+        }
+    }
     tls.write_all(b"Hello from the server\n").await?;
     tls.flush().await?;
     Ok(())
 }
-
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -34,13 +53,19 @@ async fn main() -> Result<()> {
     config.extended_key_update = true;
     let acceptor = TlsAcceptor::from(Arc::new(config));
     let listener = TcpListener::bind(format!("[::]:{}", 4443)).await?;
+    let mode = if std::env::args().any(|a| a == "--respond-to=read") {
+        RespondToMode::Record
+    } else {
+        RespondToMode::Line
+    };
+
     loop {
         let (tcp_stream, _) = listener.accept().await?;
         let acceptor = acceptor.clone();
         let fut = async move {
             let mut tls = acceptor.accept(tcp_stream).await?;
             loop {
-                match pongping(&mut tls).await {
+                match pongping(&mut tls, mode).await {
                     Ok(()) => {},
                     Err(e) if e.kind()
                         == std::io::ErrorKind::UnexpectedEof => break,
